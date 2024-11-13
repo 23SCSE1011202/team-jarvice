47682b68d50b90de933acdb038c24225634707c2
23SCSE1011202 <ratneshtiwari3213@gmail.com>
2024-11-13 13:05:26 +0530

    main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+java -version
+openjdk version "17.0.x" 202x-xx-xx
+OpenJDK Runtime Environment (build 17.0.x+xx)
+OpenJDK 64-Bit Server VM (build 17.0.x+xx, mixed mode)
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
